069dc5964924f9a347ddb91a4ca65ece2b48e276
Error0810 <wnsgnswls1@gmail.com>
2026-01-30 20:24:34 +0900

    feat: 플레이어/몬스터 공통 엔티티 시스템 구현
    - IEntity 인터페이스 및 EntityBase 추상 클래스 추가
    - ExpressionType, WeaponType 열거형 추가 (1:1 매칭)
    - Monster 클래스 및 GoldPickup 픽업 시스템 구현
    - Player_Topdown에 IEntity 구현 (체력, 표정, 피격, 골드)
    - Input System Enable/Disable 처리 추가
    - InputAction 플레이어 전용 설정
    - 플레이어 애니메이터 4방향 블렌드 애니메이션 추가

---
+++ Assets/PersonalWorks/YJ/Scripts/Expression/ExpressionType.cs
@@ -0,0 +1,10 @@
+/// <summary>
+/// 캐릭터 표정 타입 (스탯 보너스와 연결)
+/// </summary>
+public enum ExpressionType
+{
+    Expression1,  // 공격력 +20%
+    Expression2,  // 피해 감소 +20%
+    Expression3,  // 이동속도 +20%
+    Expression4   // 전체 +5%
+}
